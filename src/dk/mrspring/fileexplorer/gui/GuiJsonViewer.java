8d9095c4796055a3a5fe2370f801886a98b5670b
Konrad Rasmussen <MrSpring@mrspring.dk>
2014-12-27 17:10:10 +0100

    Syncnig code

--- src/dk/mrspring/fileexplorer/gui/GuiJsonViewer.java
+++ src/dk/mrspring/fileexplorer/gui/GuiJsonViewer.java
@@ -1,10 +1,10 @@
 package dk.mrspring.fileexplorer.gui;
 
 import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
 import com.google.gson.reflect.TypeToken;
-import dk.mrspring.fileexplorer.gui.helper.Color;
-import dk.mrspring.fileexplorer.gui.helper.DrawingHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -51,32 +51,44 @@ public class GuiJsonViewer implements IGui
         int yOffset = 0;
         for (Map.Entry<String, Object> entry : this.jsonObject.entrySet())
         {
-            this.drawObject(minecraft, 0, yOffset, entry.getKey(), entry.getValue());
-            yOffset += 12;
+            yOffset += this.drawObject(minecraft, 0, yOffset, entry.getKey(), entry.getValue()) + 2;
         }
     }
 
-    private void drawObject(Minecraft minecraft, int xOffset, int yOffset, String name, Object object)
+    /**
+     * Renders the Object and returns its display height.
+     *
+     * @param minecraft The Minecraft game object, used to get the Font Renderer for rendering text.
+     * @param xOffset   X offset to render the object at, used for indentation.
+     * @param yOffset   Y offset to render the object at.
+     * @param name      The name of the object being rendered.
+     * @param object    The object being rendered.
+     * @return Returns the height of the rendered object, which gets added to the yOffset to render the next object below this.
+     */
+    private int drawObject(Minecraft minecraft, int xOffset, int yOffset, String name, Object object)
     {
-//        System.out.println("Drawing object: " + name + ", of type: " + object.getClass().getName());
-        if (object instanceof String)
+//        System.out.println("Drawing object: " + name + ", of type: " + object.getClass().getName() + " at Y: " + yOffset);
+        if (object instanceof String || object instanceof Boolean || object instanceof Double)
         {
-            String string = (String) object;
-            minecraft.fontRendererObj.drawString(name + ": " + string, x + xOffset, y + yOffset, 0xFFFFFF, true);
-        } else if (object instanceof Boolean)
-        {
-            boolean bool = (Boolean) object;
-            minecraft.fontRendererObj.drawString(name + ": " + String.valueOf(bool), x + xOffset, y + yOffset, 0xFFFFFF, true);
-        } else if (object instanceof Double)
-        {
-            double doub = (Double) object;
-            minecraft.fontRendererObj.drawString(name + ": " + String.valueOf(doub), x + xOffset, y + yOffset, 0xFFFFFF, true);
+            this.drawSimpleTextValue(minecraft.fontRendererObj, name, object, x + xOffset, y + yOffset);
+            return 10;
         } else if (object instanceof ArrayList)
         {
-            ArrayList list = (ArrayList) object;
+            ArrayList<Object> list = (ArrayList<Object>) object;
+            int height = 10;
+
+            minecraft.fontRendererObj.drawString(name + ":", x + xOffset, y + yOffset, 0xFFFFFF, true);
+            for (int i = 0; i < list.size(); i++)
+            {
+                this.drawSimpleTextValue(minecraft.fontRendererObj, String.valueOf(i), list.get(i), x + xOffset + 5, y + yOffset + height);
+                height += 10;
+            }
+            return height;
+
+            /*ArrayList list = (ArrayList) object;
             minecraft.fontRendererObj.drawString(name + ":", x + xOffset, y + yOffset, 0xFFFFFF, true);
             yOffset += 10;
-            DrawingHelper.drawQuad(x + xOffset + 1, y + yOffset+1, 1, 3l, Color.DKGREY, 1F);
+            DrawingHelper.drawQuad(x + xOffset + 1, y + yOffset + 1, 1, 3l, Color.DKGREY, 1F);
             int yOffsetAtListStart = yOffset;
             for (int i = 0, listSize = list.size(); i < listSize; i++)
             {
@@ -86,10 +98,38 @@ public class GuiJsonViewer implements IGui
                     DrawingHelper.drawQuad(x + xOffset + 1, y + yOffset + 5, 1, 9, Color.DKGREY, 1F);
                 Object listObject = list.get(i);
                 this.drawObject(minecraft, xOffset + 5, yOffset, String.valueOf(i), listObject);
+                if (i < listSize - 1)
+                    yOffset += 10;
+            }
+            DrawingHelper.drawQuad(x + xOffset, y + yOffsetAtListStart, 1, yOffset - yOffsetAtListStart + 4, Color.WHITE, 1F);*/
+        } else if (object instanceof LinkedTreeMap)
+        {
+            LinkedTreeMap<String, Object> treeMap = (LinkedTreeMap<String, Object>) object;
+            minecraft.fontRendererObj.drawString(name + ":", x + xOffset, y + yOffset, 0xFFFFFF, true);
+            yOffset += 10;
+            for (Map.Entry<String, Object> entry : treeMap.entrySet())
+            {
+                String entryName = entry.getKey();
+                Object entryObject = entry.getValue();
+                this.drawObject(minecraft, xOffset + 5, yOffset, entryName, entryObject);
                 yOffset += 10;
             }
-            DrawingHelper.drawQuad(x + xOffset, y + yOffsetAtListStart, 1, yOffset - yOffsetAtListStart - 6, Color.WHITE, 1F);
-        }
+            return 10;
+        } else return 0;
+    }
+
+    /**
+     * Draws a simple key, value line.
+     *
+     * @param renderer The Font Renderer object used to render the text.
+     * @param name     The name/key for the value.
+     * @param object   The value being rendered.
+     * @param x        The X position to render the key, value set.
+     * @param y        The Y position to render the key, value set.
+     */
+    private void drawSimpleTextValue(FontRenderer renderer, String name, Object object, int x, int y)
+    {
+        renderer.drawString(name + ": " + String.valueOf(object), x, y, 0xFFFFFF, true);
     }
 
     @Override
