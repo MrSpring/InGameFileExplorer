2ee275dff01b599d3b25311820c7596774e6ca7b
MrSpring <mrspring@mrspring.dk>
2015-02-17 21:55:12 +0100

    Added setters to GuiDropDownList

--- src/dk/mrspring/fileexplorer/gui/GuiDropDownList.java
+++ src/dk/mrspring/fileexplorer/gui/GuiDropDownList.java
@@ -144,6 +144,16 @@ public class GuiDropDownList implements IGui
         return x;
     }
 
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
     public static class ListElement
     {
         String text;
